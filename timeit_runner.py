e2608e38f26dec56625f0bb836dc0b06252da40c
joshvernon <jvernon_483@hotmail.com>
2018-06-27 00:17:25 -0500

    Make run_timeit more generic

--- timeit_runner.py
+++ timeit_runner.py
@@ -1,15 +1,20 @@
 import timeit
 
-def run_timeit(function_name, repeat_count=5):
-    if function_name not in ('upsert_features_sequential'):
+BASE_SETUP_CODE = "from cb_utils import connect;bucket = connect()"
+UFS_SETUP_CODE = "from load_geojson import get_features,"\
+"upsert_features_sequential;{0};features = get_features()".format(BASE_SETUP_CODE)
+
+FUNCTION_CONFIGS = {
+    'upsert_features_sequential': UFS_SETUP_CODE
+}
+
+def run_timeit(function_name, repeat_count, *args,):
+    if function_name not in (FUNCTION_CONFIGS.keys()):
         return
 
     # Setup the code to run.
-    setup_code = "from load_geojson import get_features, {0};"\
-    "from cb_utils import connect;"\
-    "features = get_features();"\
-    "bucket = connect()".format(function_name)
-    stmt_code = "{0}(bucket, features)".format(function_name)
+    setup_code = FUNCTION_CONFIGS[function_name]
+    stmt_code = "{0}({1})".format(function_name, ', '.join(args))
 
     # Run timeit.
     print('Running timeit. Hang tight...')
@@ -26,5 +31,5 @@ def run_timeit(function_name, repeat_count=5):
         print('Trial {0:d}: {1:.3f} s'.format(i, result))
 
 if __name__ == '__main__':
-    run_timeit('upsert_features_sequential')
+    run_timeit('upsert_features_sequential', 5, 'bucket', 'features')
     
