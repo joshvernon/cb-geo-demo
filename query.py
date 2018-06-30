8f46c2a3e2f382e1e1dcbccf5f1835b7bfccbcff
joshvernon <jvernon_483@hotmail.com>
2018-06-30 06:15:09 -0500

    Print out everything from query result rows

--- query.py
+++ query.py
@@ -6,7 +6,7 @@ def query_bounding_box(bucket, lower_left, upper_right):
     spatial_query = SpatialQuery(start_range=lower_left, end_range=upper_right)
     view = bucket.query('main', 'stops', use_devmode=True, query=spatial_query)
     for counter, row in enumerate(view, 1):
-        print(row.value['LOCATION'])
+        print(row.key, row.value, row.geometry)
     print('Processed {0:d} rows.'.format(counter))
 
 if __name__ == '__main__':
